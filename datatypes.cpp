887ea3acfb2fcde4d5822c25265d09c18bf57481
tazerface007 <rohandesoza@duck.com>
2025-11-05 21:32:52 +0530

    Add datatypes.cpp

---
+++ datatypes.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout << "============================ DATATYPES IN CPP ============================" << endl;
+//	short vshort = 32769;
+	short vshort = 32767;
+	cout << "Short: " << vshort << endl;
+	return 0;
+}
